84b89e5a307c3ccc237c7c65c8abac39bebca192
dhruvawani17 <dhruvawani17@gmail.com>
2026-07-04 10:41:53 +0530

    Add new files

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Add backend to path so we can import the app
+# Add backend to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
 
 from mangum import Mangum
